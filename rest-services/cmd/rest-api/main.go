7736dfd06199669afcc0400079c4be45243ad128
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-02-07 16:37:20 -0500

    Added todo comments

--- rest-services/cmd/rest-api/main.go
+++ rest-services/cmd/rest-api/main.go
@@ -10,6 +10,20 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.RequestLogger())
+
+	//TODO: Endpoints
+	// Verify and Update config.txt enable pcie gen3
+	// Compile the linux kernel -
+	//     a. download linux
+	//     b. update the kernel config file --> enable nvme-fa options
+	//     c. stretch goal - create a slim version of the kernel config txt file.. get rid of stuff we dont need.
+	// Check see if any header packages are installed if so removed them - this messes up zfs install
+	// Install ZFS
+	// Get Drive Telemetry - get the installed nvme drive data with serial number so we can build the pool from drive SN
+	// Create PiNAS working partition(1-5G?).. We will need a working partition to store data, install dns, step-ca and move /etc off the micro-sd
+	// Create zpool api
+	// Create datasets - Need to look at the options and support what we need. nvme-fa block storage we will need
+
 	e.GET("/cpuInfo", system_info.GetCpu)
 
 	// Start the server
