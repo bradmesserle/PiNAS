e072be535bb8c9e8a2baec3449b07a174d8f3dbf
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-02-14 21:15:01 -0500

    Updates to kernel compilation rest-api

--- rest-services/internal/kernel-compile/compile-kernel.go
+++ rest-services/internal/kernel-compile/compile-kernel.go
@@ -1,6 +1,7 @@
 package kernel_compile
 
 import (
+	"bytes"
 	"log"
 	"net/http"
 	"os"
@@ -9,13 +10,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var buildDir = "/kernel-build"
+var linuxDir = buildDir + "/linux"
+
 // CompileLinuxKernel Compile the linux kernel we need to enable the nvme-fa options
 // Once compiled and installed, we will need to remove any header packages that might be left behind and
 // reboot the system
 func CompileLinuxKernel(c echo.Context) error {
 
 	//Create a work area
-	if err := os.Mkdir("/kernel-build", os.ModePerm); err != nil {
+	if err := os.Mkdir(buildDir, os.ModePerm); err != nil {
 		// os.ModePerm is equivalent to 0777 on most systems, then modified by umask
 		log.Println(err)
 		//return c.JSON(http.StatusInternalServerError, "")
@@ -32,12 +36,16 @@ func CompileLinuxKernel(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, "Error cloning linux kernel repo")
 	}
 
-	//Configure the kernel build config.
-
 	//Prep the kernel build
 	prepareErr := PrepareKernelBuild()
 	if prepareErr != nil {
 		return c.JSON(http.StatusInternalServerError, "Error preparing kernel build")
+	}
+
+	//Configure the kernel build config.
+	configErr := UpdateBuildConfig()
+	if configErr != nil {
+		return c.JSON(http.StatusInternalServerError, "Error configuring kernel build")
 	}
 
 	//Build the kernel
@@ -47,6 +55,10 @@ func CompileLinuxKernel(c echo.Context) error {
 	}
 
 	//Install the kernel
+	installErr := InstallKernel()
+	if installErr != nil {
+		return c.JSON(http.StatusInternalServerError, "Error installing kernel")
+	}
 
 	return c.JSON(http.StatusOK, "Built and installed the linux kernel successfully")
 }
@@ -70,15 +82,15 @@ func InstallDevTools() error {
 func CloneLinuxKernel() error {
 
 	//Need to check if the directory exists, if so, delete it
-	if _, err := os.Stat("/kernel-build/linux"); err == nil {
-		err := os.RemoveAll("/kernel-build/linux")
+	if _, err := os.Stat(linuxDir); err == nil {
+		err := os.RemoveAll(linuxDir)
 		if err != nil {
 			log.Println(err)
 		}
 	}
 
 	cloneCmd := exec.Command("git", "clone", "--depth=1", "https://github.com/raspberrypi/linux")
-	cloneCmd.Dir = "/kernel-build"
+	cloneCmd.Dir = buildDir
 	cloneCmd.Stdout = os.Stdout
 	cloneCmd.Stderr = os.Stderr
 	cloneErr := cloneCmd.Run()
@@ -92,7 +104,7 @@ func CloneLinuxKernel() error {
 // PrepareKernelBuild Configure the kernel build config.
 func PrepareKernelBuild() error {
 	cmd := exec.Command("make", "bcm2712_defconfig")
-	cmd.Dir = "/kernel-build/linux"
+	cmd.Dir = linuxDir
 	cmd.Env = append(os.Environ(), "KERNEL=kernel_2712")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -104,10 +116,41 @@ func PrepareKernelBuild() error {
 	return nil
 }
 
+// UpdateBuildConfig Enable nvme-fa options
+func UpdateBuildConfig() error {
+
+	var buf bytes.Buffer
+	buf.WriteString("CONFIG_TLS=m\n")
+	buf.WriteString("CONFIG_STREAM_PARSER=y\n")
+	buf.WriteString("CONFIG_NVME_KEYRING=m\n")
+	buf.WriteString("CONFIG_NVME_FABRICS=m\n")
+	buf.WriteString("CONFIG_NVME_TCP=m\n")
+	buf.WriteString("CONFIG_NVME_TCP_TLS=y\n")
+
+	//Create a config fragment file
+	fileErr := os.WriteFile(linuxDir+"/kernel/configs/nvme_fa.config", buf.Bytes(), 0644)
+	if fileErr != nil {
+		return fileErr
+	}
+
+	//Update the kernel build config with the nvme-fa options
+	cmd := exec.Command("make", "nvme_fa.config")
+	cmd.Dir = linuxDir
+	cmd.Env = append(os.Environ(), "KERNEL=kernel_2712")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	runErr := cmd.Run()
+	if runErr != nil {
+		return runErr
+	}
+
+	return nil
+}
+
 // BuildKernel Build the kernel
 func BuildKernel() error {
 	cmd := exec.Command("make", "-j6", "Image.gz", "modules", "dtbs")
-	cmd.Dir = "/kernel-build/linux"
+	cmd.Dir = linuxDir
 	cmd.Env = append(os.Environ(), "KERNEL=kernel_2712")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -116,6 +159,12 @@ func BuildKernel() error {
 		log.Println(err)
 		return err
 	}
+
+	return nil
+}
+
+// InstallKernel Install the kernel
+func InstallKernel() error {
 
 	return nil
 }
