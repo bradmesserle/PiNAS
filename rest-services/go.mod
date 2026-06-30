d272a6cb3f0bd35480c88d7f2bbf27fcb1af1d52
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-06-30 12:55:00 -0400

    Test SSE Events

--- rest-services/go.mod
+++ rest-services/go.mod
@@ -5,7 +5,6 @@ go 1.26.1
 require (
 	github.com/labstack/echo/v5 v5.2.1
 	github.com/pinas/common-structs v0.0.0-00010101000000-000000000000
-	github.com/r3labs/sse/v2 v2.10.0
 )
 
 replace github.com/pinas/common-structs => ../common-structs
@@ -13,5 +12,4 @@ replace github.com/pinas/common-structs => ../common-structs
 require (
 	golang.org/x/net v0.56.0 // indirect
 	golang.org/x/time v0.15.0 // indirect
-	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
 )
--- rest-services/internal/system-updates/apt-update.go
+++ rest-services/internal/system-updates/apt-update.go
@@ -1,13 +1,12 @@
 package system_updates
 
 import (
+	"bufio"
 	"fmt"
 	"log"
-	"net/http"
-	"time"
+	"os/exec"
 
 	"github.com/labstack/echo/v5"
-	"github.com/pinas/rest-services/internal/utilities"
 )
 
 // AptUpdate Update the apt cache
@@ -20,25 +19,53 @@ func AptUpdate(c *echo.Context) error {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-	count := uint64(0)
-	for {
-		select {
-		case <-c.Request().Context().Done():
-			log.Printf("SSE client disconnected, ip: %v", c.RealIP())
-			return nil
-		case <-ticker.C:
-			count++
-			event := utilities.Event{
-				Data: []byte(fmt.Sprintf("count: %d, time: %s\n\n", count, time.Now().Format(time.RFC3339Nano))),
-			}
-			if err := event.MarshalTo(w); err != nil {
-				return err
-			}
-			if err := http.NewResponseController(w).Flush(); err != nil {
-				return err
-			}
-		}
+	cmd := exec.Command("sudo", "apt-get", "update")
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		// Prints each line immediately as it is generated
+		fmt.Println("Streamed line:", scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Printf("Scanner error: %v", err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
+
+	//ticker := time.NewTicker(1 * time.Second)
+	//defer ticker.Stop()
+	//count := uint64(0)
+	//for {
+	//	select {
+	//	case <-c.Request().Context().Done():
+	//		log.Printf("SSE client disconnected, ip: %v", c.RealIP())
+	//		return nil
+	//	case <-ticker.C:
+	//		count++
+	//		event := utilities.Event{
+	//			Data: []byte(fmt.Sprintf("count: %d, time: %s\n\n", count, time.Now().Format(time.RFC3339Nano))),
+	//		}
+	//		if err := event.MarshalTo(w); err != nil {
+	//			return err
+	//		}
+	//		if err := http.NewResponseController(w).Flush(); err != nil {
+	//			return err
+	//		}
+	//	}
+	//}
+
+	return nil
+
 }
