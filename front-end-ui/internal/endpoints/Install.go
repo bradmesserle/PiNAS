111cc8109a30483c2e03fc49e42ea43c5dc8cb7e
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-07-01 01:09:20 -0400

    Updates for installing zfs

---
+++ front-end-ui/internal/endpoints/Install.go
@@ -0,0 +1,17 @@
+package endpoints
+
+import (
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v4"
+	"github.com/pinas/ui/internal/components/setup"
+	"github.com/pinas/ui/internal/structs"
+)
+
+func Install(c echo.Context, wizardInfo *structs.WizardInfo) error {
+
+	//Kick off the install process
+
+	var cmp templ.Component = setup.InstallProgressPage(*wizardInfo)
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
+	return cmp.Render(c.Request().Context(), c.Response().Writer)
+}
