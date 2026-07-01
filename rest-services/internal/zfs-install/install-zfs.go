f2c9130cf97da487d9eb637b0092bb38016f9002
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-07-01 01:20:16 -0400

    Updates for installing zfs

--- rest-services/internal/zfs-install/install-zfs.go
+++ rest-services/internal/zfs-install/install-zfs.go
@@ -28,7 +28,7 @@ func InstallZfs(c *echo.Context) error {
 // CheckForInstalledHeadersAndRemoveThem checks if the kernel headers packages are installed
 func CheckForInstalledHeadersAndRemoveThem() error {
 
-	out, err := exec.Command("dpkg", "--get-selections", "|", "grep,", "linux-headers").Output()
+	out, err := exec.Command("dpkg", "--get-selections").Output()
 	if err != nil {
 		log.Println(err)
 		return err
