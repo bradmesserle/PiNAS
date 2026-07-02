8cb74e77a384a322634aabd272a92727fadbf10f
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-07-02 00:51:45 -0400

    Added DEBIAN_FRONTEND=noninteractive to the install apt command line to avoid interactive prompts

--- rest-services/internal/zfs-install/install-zfs.go
+++ rest-services/internal/zfs-install/install-zfs.go
@@ -116,7 +116,7 @@ func aptInstallZfs(w http.ResponseWriter) error {
 		slog.Error("Error while installing zfs", "Value", err)
 	}
 
-	cmd := exec.Command("DEBIAN_FRONTEND=noninteractive", "apt", "install", "zfs-dkms", "zfsutils-linux", "-y")
+	cmd := exec.Command("sudo", "DEBIAN_FRONTEND=noninteractive", "apt", "install", "zfs-dkms", "zfsutils-linux", "-y")
 	err := utilities.ExecCmdSseStdoutText(cmd, w)
 	if err != nil {
 		slog.Error("Error while installing zfs", "Value", err)
