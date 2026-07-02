bb46d9109516fdff267578ce2fecb2304ab7ff2c
bradmesserle <228630+bradmesserle@users.noreply.github.com>
2026-07-02 13:00:07 -0400

    Added and Fixed Logging

--- rest-services/internal/system-updates/apt-update.go
+++ rest-services/internal/system-updates/apt-update.go
@@ -19,11 +19,16 @@ func AptUpdate(c *echo.Context) error {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
+	log.Printf("Updating APT Repositories")
+	if err := utilities.SendEventData("Updating APT Repositories", "consoleOutput", w); err != nil {
+		slog.Error("Error while sending event data", "Value", err)
+	}
+
 	cmd := exec.Command("sudo", "apt-get", "update", "-y")
 
 	err := utilities.ExecCmdSseStdoutText(cmd, w)
 	if err != nil {
-		slog.Error("Error while running apt update", err)
+		slog.Error("Error while running apt update", "Value", err)
 		return err
 	}
 
